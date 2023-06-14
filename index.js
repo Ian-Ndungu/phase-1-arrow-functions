f047bafe04c71bcd5745fa957cdcd237c91d1f20
Ian Ndungu <ianndungu06@gmail.com>
2023-06-14 12:50:21 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+let divide = function (numa = 2000, numb = 100, divides){
+       divides = numa/numb
+    return divides;
+}
+divide();
+
+let square = (num1 = 2, squared)=>{
+     squared = num1*num1
+    return squared;
+}
+square();
+
+let add = (num3 =5, num4 = 7, addition)=>{
+     addition = num3+num4
+    return addition;
+}
+add();
